1740da02e43af51b25c3e06c327f64002f09ad67
Culda, A.D. (Alexandru, Student B-BIT) <a.culda@student.utwente.nl>
2023-10-16 00:56:27 +0200

    Minor additions to the output; The name of the picture appears now for every entry in the terminal/file.

--- Start.py
+++ Start.py
@@ -9,29 +9,31 @@ def main():
         output_loc = input("Where do you want to print the output?\n\n1.File\n2.Terminal\n\nInput: ")
         output_loc = int(output_loc) # Conversion from a string (input) to int
         if (output_loc == 1):
-            # open = redirect output to the file
-            sys.stdout = open("exif_data_extract.txt", "w")
+            sys.stdout = open("exif_data_extract.txt", "w") # open = redirect output to the file
             break
         elif (output_loc == 2):
-            for image in pictures:
-                with Image.open(image) as im:
-                    print(im.getexif())
-                    print(im.info)
-                    print(im)
-                    Image.Image.load(im)
-                    print("\n============\n")
+            extract_EXIF_data()
             break
         print("Wrong input. Please choose between 1 and 2.")
 
 # Current Working Directory; As of now, it is ../ImgMalwareScaner
 cwd = os.getcwd()
-
 # Chdir changes the current path to specified path
 os.chdir(os.path.join(cwd, "Pictures"))
-
 pictures = os.listdir()
-print(pictures)
 
-
-
+def extract_EXIF_data():
+        for picture in pictures:
+            try:
+                with Image.open(picture) as im:
+                    print(f"\n\n====== {picture} ======")
+                    print(im.getexif())
+                    print(im.info)
+                    print(im)
+                    Image.Image.load(im)
+            except IOError:
+                print("Image format is not supported.")
 main()
+
+
+
